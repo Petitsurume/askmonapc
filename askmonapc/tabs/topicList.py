d2505bceb5c104d36c211f73f1733ad6ad9adb0c
petitsurume <git@surume.tk>
2017-06-04 19:47:29 +0900

    とりあえず勢い表示実装

--- askmonapc/tabs/topicList.py
+++ askmonapc/tabs/topicList.py
@@ -19,14 +19,17 @@ class TopicList(QTreeView):
         self.topics = requests.get(const.API_URL+"topics/list",headers = const.HTTP_HEADERS).json().get("topics", [])
         def mapfunc(item):
             return item["title"]
-        listModel = QStandardItemModel(0, 2, self)
+        listModel = QStandardItemModel(0, 3, self)
         listModel.setHeaderData(0, Qt.Horizontal, "トピック名")
-        listModel.setHeaderData(1, Qt.Horizontal, "age日")
+        listModel.setHeaderData(1, Qt.Horizontal, "勢い")
+        listModel.setHeaderData(2, Qt.Horizontal, "age日")
         for topic in self.topics:
             title_item = QStandardItem(topic.get("title", "(無題)"))
             title_item.setEditable(False)
+            ikioi_item = QStandardItem(str(topic.get("count", 0) / ((int(datetime.datetime.now().strftime("%s")) - topic.get("created", 0))/60) * 60 * 24))
+            ikioi_item.setEditable(False)
             age_item = QStandardItem(datetime.datetime.fromtimestamp(topic.get("updated")).strftime("%Y/%m/%d %H:%M:%S"))
             age_item.setEditable(False)
-            listModel.appendRow([title_item, age_item])
+            listModel.appendRow([title_item, ikioi_item, age_item])
         self.setModel(listModel)
         self.resizeColumnToContents(0)
